9c965811c2e4314780334ca88486941cf98e4626
Melek Badreddine <119127830+melekbadreddine@users.noreply.github.com>
2024-12-22 14:03:10 +0100

    Delete EventService directory

--- EventService/src/main/java/com/example/project/dao/EvenementDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.project.entities.Evenement;
-
-public interface EvenementDao extends JpaRepository<Evenement,Long> {
-}
